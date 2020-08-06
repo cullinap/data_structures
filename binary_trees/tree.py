6bdcc3d4ed28fa2f95fb9ac9f97411ca45d9130e
Patrick-Cullinane <patrick.cullinane@ibm.com>
2020-08-06 10:04:01 -0400

    step 2: add pre-order traversal

--- binary_trees/tree.py
+++ binary_trees/tree.py
@@ -7,17 +7,41 @@ class Node(object):
 class BinaryTree(object):
     def __init__(self,root):
         self.root = Node(root)
+        
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported")
+        
+    def preorder_print(self, start, traversal):
+        """Root->Left->Right"""
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+            
+        return traversal
 
+    
+    
 #         1
 #       /   \
 #      2     3
-#     / \   
-#    4   5  
+#     / \    / \ 
+#    4   5  6   7
         
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
 
-# another level
+# left branch
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
+
+# right branch
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+tree.print_tree("preorder")
